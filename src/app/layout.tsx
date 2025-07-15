0c476b42a6ef05c16bfc31e3adf08ee2ed0dfb1e
Ianana1111 <ian52759@gmail.com>
2025-07-15 16:41:37 +0800

    change the font family

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
         </Script>
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased font-helvetica`}
       >
         <LanguageProvider>
           {children}
